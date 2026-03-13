3413021d81c0a6344dd25e9ac0b6e2a4ebce6696
Gregor Gatej <gregor.gatej@gmail.com>
2026-03-13 17:24:31 +0100

    feat: perform AddDeviseToUsers and ChangeEmailInUsers migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_13_154403) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_13_162154) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -22,10 +22,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_13_154403) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.string "email"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "password"
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
     t.datetime "updated_at", null: false
     t.string "username"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "posts", "users"
